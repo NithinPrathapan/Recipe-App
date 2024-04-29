24889d0edb38b5f48cabcc19f89817e64a308f99
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-04-29 11:28:41 +0530

    Navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Sections/Nav";
+
+const App = () => {
+  return (
+    <div className="">
+      <Nav />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Sections/Nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaOpencart } from "react-icons/fa";
+import { RiAccountCircleLine } from "react-icons/ri";
+import { IoSearch } from "react-icons/io5";
+
+const Nav = () => {
+  return (
+    <nav className="flex px-4 py-6 justify-between m-0 items-center ">
+      <div className="logo relative">
+        <div>
+          <h1 className="font-oswald font-semibold text-purple-900-500 text tracking-widest relative z-50  text-2xl left-6">
+            Book a Meal
+          </h1>
+        </div>
+      </div>
+      <div className="routes hidden sm:block ">
+        <div className="flex gap-2 font-oswald text-lg tracking-wider font-semibold  md:gap-8 ">
+          <a href="#home">Home</a>
+          <a href="home">About us</a>
+          <a href="home">FAQ</a>
+          <a href="home">Contact</a>
+        </div>
+      </div>
+      <div className="cart-section flex flex-row justify-between sm:gap-4 gap-2 items-center">
+        <IoSearch size={28} />
+        <FaOpencart size={28} />
+        <RiAccountCircleLine size={28} />
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
