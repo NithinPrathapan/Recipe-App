6cf4cca4fbb3cf0e06436327bed0713aa4d3de1d
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-04-30 11:28:01 +0530

    search bar

--- src/Sections/Nav.jsx
+++ src/Sections/Nav.jsx
@@ -8,21 +8,22 @@ const Nav = () => {
     <nav className="flex px-4 py-6 justify-between m-0 items-center ">
       <div className="logo relative">
         <div>
-          <h1 className="font-oswald font-semibold text-purple-900-500 text tracking-widest relative z-50  text-2xl left-6">
+          <h1 className="font-oswald font-semibold text-purple-900-500 text tracking-widest relative z-50  text-xl left-6">
             Book a Meal
           </h1>
         </div>
       </div>
-      <div className="routes hidden sm:block ">
-        <div className="flex gap-2 font-oswald text-lg tracking-wider font-semibold  md:gap-8 ">
-          <a href="#home">Home</a>
-          <a href="home">About us</a>
-          <a href="home">FAQ</a>
-          <a href="home">Contact</a>
+      <div className="routes border px-2 py-1 w-[60%] justify-between rounded-lg sm:flex hidden">
+        <input
+          className="outline-none"
+          type="text"
+          placeholder="Type Ingredients..."
+        />
+        <div className="bg-orange-500 p-1 rounded-lg">
+          <IoSearch className="" color="white" size={24} />
         </div>
       </div>
-      <div className="cart-section flex flex-row justify-between sm:gap-4 gap-2 items-center">
-        <IoSearch size={28} />
+      <div className="cart-section  flex flex-row  sm:gap-4 gap-2 items-center">
         <FaOpencart size={28} />
         <RiAccountCircleLine size={28} />
       </div>
